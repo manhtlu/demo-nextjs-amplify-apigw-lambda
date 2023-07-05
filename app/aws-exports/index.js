39b9b4a33fa046847ebcbf18692e186c3666a26c
manh.nguyenthe <manh.nguyenthe@vietis.com.vn>
2023-07-05 14:26:15 +0700

    init

---
+++ app/aws-exports/index.js
@@ -0,0 +1,9 @@
+export default {
+    aws_project_region: 'ap-southeast-1',
+    aws_cognito_region: 'ap-southeast-1',
+    aws_user_pools_id: 'ap-southeast-1_CERQCSD2S',
+    aws_user_pools_web_client_id: '4t2gil1710uk95sd6k8156bubh',
+    aws_cognito_identity_pool_id:
+        'us-east-1:f602c14b-0fde-409c-9a7e-0baccbfd87d0',
+    aws_mandatory_sign_in: 'enable'
+}
--- app/page.js
+++ app/page.js
@@ -1,94 +1,86 @@
-import Image from 'next/image'
+'use client'
+import { API, Amplify, Auth } from 'aws-amplify';
 import styles from './page.module.css'
-
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+Amplify.configure({
+  aws_project_region: 'ap-southeast-1',
+  aws_cognito_region: 'ap-southeast-1',
+  aws_user_pools_id: 'ap-southeast-1_CERQCSD2S',
+  aws_user_pools_web_client_id: '4t2gil1710uk95sd6k8156bubh',
+  aws_cognito_identity_pool_id:
+    'us-east-1:f602c14b-0fde-409c-9a7e-0baccbfd87d0',
+  aws_mandatory_sign_in: 'enable',
+  aws_cloud_logic_custom: [
+    {
+      name: 'api-nextjs-serverless',
+      endpoint: 'https://twu0jqizo3.execute-api.ap-southeast-1.amazonaws.com/dev',
+      region: 'ap-southeast-1',
+    }
+  ]
+});
 export default function Home() {
+
+  const postData = async () => {
+    const user = await getUserData()
+    const idToken = user.signInUserSession.idToken.jwtToken;
+    const myInit = {
+      headers: {
+        Authorization: idToken
+      },
+      body: {
+        email: user.attributes.email,
+        name: user.attributes.name,
+      },
+      response: true,
+    };
+    const data = await API.post('api-nextjs-serverless', '/hello', myInit)
+
+    console.log(data)
+  }
+
+  const getData = async () => {
+    const user = await getUserData()
+    const idToken = user.signInUserSession.idToken.jwtToken;
+    const myInit = {
+      headers: {
+        Authorization: idToken
+      },
+      response: true,
+    };
+    const data = await API.get('api-nextjs-serverless', '/hello', myInit)
+
+    console.log(data)
+  }
+
+  const getUserData = async () => {
+    const user = await Auth.currentAuthenticatedUser()
+
+    return user;
+  }
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <h1>Serverless App</h1>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div>
+          <Authenticator
+            loginMechanisms={['email']}
+            signUpAttributes={['name']}
+            socialProviders={['google']}>
+            {({ signOut, user }) => (
+              <main>
+                <h1>Hello {user.attributes.name} ({user.attributes.email})</h1>
+                <button onClick={getUserData}>Call API</button>
+                <button onClick={getData}>Call Data</button>
+                <button onClick={signOut}>Sign out</button>
+                <div><button onClick={postData}>Post Data</button></div>
+              </main>
+            )}
+          </Authenticator>
+        </div>
       </div>
     </main>
   )
